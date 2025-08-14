5d1448640a86c798d07523afb74eb816c4d50aaa
ronnieras9533 <ronnieras9533@gmail.com>
2025-08-14 17:11:19 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,16 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development' && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ["react-chartjs-2", "chart.js"], // <--- Add this line
     },
   },
 }));
